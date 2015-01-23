6ad672cecc65e2f42fb640b4bf51086318ccd1af
John Hauck <john_hauck@lecotc.com>
2015-01-23 12:16:15 -0500

    New project created with Visual Studio 2013 Update 4 and Xamarin 3.9.236.0

---
+++ XamarinFormsBug01/XamarinFormsBug01.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using XamarinFormsBug01.WinPhone.Resources;
+
+namespace XamarinFormsBug01.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
